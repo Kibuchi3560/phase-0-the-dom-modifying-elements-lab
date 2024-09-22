9bb03b2d7431fbfd6004ed778485eaa071ff0027
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-09-22 09:41:18 +0300

    Modifies DOM Element

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.querySelector("h1");
+newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Kibuchi is the champion";
